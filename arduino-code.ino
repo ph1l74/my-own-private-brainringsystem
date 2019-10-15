5a6d5b5e214869812454840f957855aac52fd625
Filat Astakhov <astakhovfilat@gmail.com>
2019-10-15 14:11:30 +0500

    Добавлены картинки, дописана статья (до кода) и обновлен код.

--- arduino-code.ino
+++ arduino-code.ino
@@ -1,57 +1,139 @@
 int ledOne = 15;    // LED1 connected to Analog Pin 1 (A1) 
-int ledTwo = 17;    // LED2 connected to Analog Pin 3 (A3) 
-int ledThree = 19;  // LED3 connected to Analog Pin 5 (A5) 
+int ledTwo = 16;    // LED2 connected to Analog Pin 2 (A2) 
+int ledThree = 17;  // LED3 connected to Analog Pin 3 (A3) 
+int ledFour = 18;    // LED4 connected to Analog Pin 4 (A4) 
+int ledFive = 19;  // LED5 connected to Analog Pin 5 (A5) 
 
-int ledMonitor = 14; 
+int ledMonitor = 14; // LEDSTART connected to Analog Pin 0 (A0)
 
-int btnOne = 4;    // BUTTON1 connected to Digital Pin 3 (A1) 
-int btnTwo = 5;    // BUTTON2 connected to Digital Pin 5 (A3) 
-int btnThree = 7;  // BUTTON3 connected to Digital Pin 7 (A5) 
+int btnOne = 4;    // BUTTON1 connected to Digital Pin 4 (D4) 
+int btnTwo = 5;    // BUTTON2 connected to Digital Pin 5 (D5) 
+int btnThree = 6;  // BUTTON3 connected to Digital Pin 6 (D6) 
+int btnFour = 7;    // BUTTON4 connected to Digital Pin 7 (D7) 
+int btnFive = 8;  // BUTTON5 connected to Digital Pin 8 (D8) 
 
-int btnStart = 2;    // BUTTONSTART connected to Digital Pin 2 (A1) 
-int btnStop = 3;    // BUTTONSTOP connected to Digital Pin 3 (A1) 
+int btnStart = 2;    // BUTTONSTART connected to Digital Pin 2 (D2) 
+int btnStop = 3;    // BUTTONSTOP connected to Digital Pin 3 (D3) 
 
 boolean timerStarted = false;
 
-int timeLimit = 30000;
+int delayForLED = 20; // IN MSECS
+unsigned long timeLimit = 300000; // IN MSECS
 unsigned long previousMillis = 0;
-
 
 void setup() {
   Serial.begin(9600);
+  analogWrite(ledOne, 255);
+  delay(delayForLED);
+  analogWrite(ledOne, 0);
+  delay(delayForLED);
+  analogWrite(ledTwo, 255);
+  delay(delayForLED);
+  analogWrite(ledTwo, 0);
+  delay(delayForLED);
+  analogWrite(ledThree, 255);
+  delay(delayForLED);
+  analogWrite(ledThree, 0);
+  delay(delayForLED);
+  analogWrite(ledFour, 255);
+  delay(delayForLED);
+  analogWrite(ledFour, 0);
+  delay(delayForLED);
+  analogWrite(ledFive, 255);
+  delay(delayForLED);
+  analogWrite(ledFive, 0);
+  delay(delayForLED);
+  analogWrite(ledFour, 255);
+  delay(delayForLED);
+  analogWrite(ledFour, 0);
+  delay(delayForLED);
+  analogWrite(ledThree, 255);
+  delay(delayForLED);
+  analogWrite(ledThree, 0);
+  delay(delayForLED);
+  analogWrite(ledTwo, 255);
+  delay(delayForLED);
+  analogWrite(ledTwo, 0);
+  delay(delayForLED);
+  analogWrite(ledOne, 255);
+  delay(delayForLED);
+  analogWrite(ledOne, 0);
+  Serial.println(timeLimit);
 }
 
 void loop() {
 
-  unsigned long currentMillis = millis();
-
-  if(digitalRead(btnStart)){
-//    Serial.println("btnStart pressed...");
+  if(digitalRead(btnStart) && !timerStarted){
     startTimer();
     previousMillis = millis();
   }
-  if (digitalRead(btnStop)){
-//    Serial.println("btnStop pressed...");
-    stopTimer();
-    previousMillis = millis();
-  }
   if(timerStarted){
-    if((unsigned long)(currentMillis - previousMillis) > timeLimit){
-      Serial.println(previousMillis);
+    if((unsigned long)(millis() - previousMillis) < timeLimit){
+
+      checkButtons();
     }
     else {
-      Serial.println("Time passed");
       stopTimer();
+      Serial.println("Time passed");
     }
   } 
 }
 
+void checkButtons() {
+  if(digitalRead(btnOne)){
+    stopTimer();
+    analogWrite(ledOne, 255);
+    Serial.print("Player one pressed in ");
+    Serial.print((millis() - previousMillis)/1000);
+    Serial.println(" sec.");
+  }
+  else if (digitalRead(btnTwo)) {
+    stopTimer();
+    analogWrite(ledTwo, 255);
+    Serial.print("Player two pressed in ");
+    Serial.print((millis() - previousMillis)/1000);
+    Serial.println(" sec.");
+  }
+  else if (digitalRead(btnThree)) {
+    stopTimer();
+    analogWrite(ledThree, 255);
+    Serial.print("Player three pressed in ");
+    Serial.print((millis() - previousMillis)/1000);
+    Serial.println(" sec.");
+  }
+    else if (digitalRead(btnFour)) {
+    stopTimer();
+    analogWrite(ledFour, 255);
+    Serial.print("Player four pressed in ");
+    Serial.print((millis() - previousMillis)/1000);
+    Serial.println(" sec.");
+  }
+    else if (digitalRead(btnFive)) {
+    stopTimer();
+    analogWrite(ledFive, 255);
+    Serial.print("Player five pressed in ");
+    Serial.print((millis() - previousMillis)/1000);
+    Serial.println(" sec.");
+  }
+}
+
 void startTimer() {
   timerStarted = true;
-  Serial.println("Timer started...");
+  turnOffLights();
+  analogWrite(ledMonitor, 255);
+  Serial.println("status:timer_started");
 }
 
 void stopTimer() {
   timerStarted = false;
-  Serial.println("Timer stopped.");
+  analogWrite(ledMonitor, 0);
+  Serial.println("status:timer_stopped");
+}
+
+void turnOffLights() {
+  analogWrite(ledOne, 0);
+  analogWrite(ledTwo, 0);
+  analogWrite(ledThree, 0);
+  analogWrite(ledFour, 0);
+  analogWrite(ledFive, 0);
 }
